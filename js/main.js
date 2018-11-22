2f34e26666b3a5c65c8b494c7045a85f34b97ed8
Shameel Kadannamanna <shameelsadaka@gmail.com>
2018-11-23 00:55:43 +0530

    Some Updates

--- js/main.js
+++ js/main.js
@@ -136,15 +136,16 @@
             $folioItems.each(function(i) {
 
                 $(this).on('click', function(e) {
-                    e.preventDefault();
-                    var options = {
-                        index: i,
-                        showHideOpacity: true
-                    }
+                    
+                    // e.preventDefault();
+                    // var options = {
+                    //     index: i,
+                    //     showHideOpacity: true
+                    // }
 
-                    // initialize PhotoSwipe
-                    var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
-                    lightBox.init();
+                    // // initialize PhotoSwipe
+                    // var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
+                    // lightBox.init();
                 });
 
             });
